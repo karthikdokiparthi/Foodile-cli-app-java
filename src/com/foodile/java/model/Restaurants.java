5b4bc04cfc99c8a2efc1bbbe2e0934306dcad70e
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-07-06 16:47:29 +0530

    chore: Add Restaurants java file

---
+++ src/com/foodile/java/model/Restaurants.java
@@ -0,0 +1,69 @@
+package com.foodile.java.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Restaurants {
+    private String id;
+    private String name;
+    private String address;
+    private List<String> menu;
+
+    public Restaurants() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<String> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<String> menu) {
+        this.menu = menu;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurants that = (Restaurants) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(address, that.address) && Objects.equals(menu, that.menu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, menu);
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurants{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", menu=" + menu +
+                '}';
+    }
+}
