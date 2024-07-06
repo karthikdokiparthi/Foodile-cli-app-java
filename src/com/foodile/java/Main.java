aaa942fe6bb43c0216aa2693759f021ea879db83
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-07-06 19:56:16 +0530

    chore: Add CsvReader in it  adding Dish data

---
+++ src/com/foodile/java/Main.java
@@ -0,0 +1,11 @@
+package com.foodile.java;
+
+import com.foodile.java.util.CsvReader;
+
+public class Main {
+    public static void main(String[] args) {
+        CsvReader csvReader = new CsvReader();
+        System.out.println(csvReader.readCsvFromCustomer());
+        System.out.println(csvReader.readCsvFromDish());
+    }
+}
--- src/com/foodile/java/util/CsvReader.java
+++ src/com/foodile/java/util/CsvReader.java
@@ -1,6 +1,7 @@
 package com.foodile.java.util;
 
 import com.foodile.java.model.Customers;
+import com.foodile.java.model.Dish;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,10 +14,10 @@ public class CsvReader {
 
     String customerFilePath="D:\\TrainingHub\\Data Types\\foodile-cle-app-java\\data\\customers.csv";
     String line;
+    String csvSplitBy=",";
     List<Customers> customersList=new ArrayList<>();
     public List<Customers> readCsvFromCustomer(){
        try (BufferedReader br=new BufferedReader(new FileReader(customerFilePath))){
-           String csvSplitBy=",";
            br.readLine();
            while ((line = br.readLine()) != null){
                String[] data = line.split(csvSplitBy);
@@ -33,5 +34,28 @@ public class CsvReader {
            System.exit(0);
        };
        return customersList;
+    }
+
+    List<Dish> dishList = new ArrayList<>();
+    String dishFilePath="D:\\TrainingHub\\Data Types\\foodile-cle-app-java\\data\\dishes.csv";
+    public List<Dish> readCsvFromDish(){
+        try (BufferedReader br=new BufferedReader(new FileReader(dishFilePath))){
+            br.readLine();
+            while ((line = br.readLine()) != null){
+                String[] data= line.split(csvSplitBy);
+                Dish dish=new Dish();
+                dish.setId(data[0]);
+                dish.setName(data[1]);
+                dish.setDescription(data[2]);
+                dish.setPrice(Double.parseDouble(data[3]));
+                dishList.add(dish);
+            }
+
+        }catch(IOException e){
+            System.out.println("File not found in the specified path");
+            System.exit(0);
+        }
+
+        return dishList;
     }
 }
