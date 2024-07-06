08135d5f0aa2b245882f3fdbe979160c23b86ceb
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-07-06 16:42:54 +0530

    chore: Add Customers java file

---
+++ src/com/foodile/java/model/Customers.java
@@ -0,0 +1,69 @@
+package com.foodile.java.model;
+
+import java.util.Objects;
+
+public class Customers {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Customers() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customers customers = (Customers) o;
+        return Objects.equals(id, customers.id) && Objects.equals(name, customers.name) && Objects.equals(email, customers.email) && Objects.equals(password, customers.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Customers{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
