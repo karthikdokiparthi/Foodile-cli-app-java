b583e5bb2b2feb7e1cdf9fd29506eb1342afffd4
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-07-07 19:32:52 +0530

    chore: In Service layer adding the CustomerService and also adding CustomerServiceImpl

---
+++ src/com/foodile/java/service/CustomerServiceImpl.java
@@ -0,0 +1,22 @@
+package com.foodile.java.service;
+
+import com.foodile.java.exceptions.CustomerExitsException;
+import com.foodile.java.model.Customers;
+import com.foodile.java.repository.CustomerRepository;
+
+import java.util.Optional;
+
+public class CustomerServiceImpl implements CustomerService{
+    private CustomerRepository customerRepository;
+    public CustomerServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository=customerRepository;
+    }
+
+    @Override
+    public Customers save(Customers customers) throws CustomerExitsException {
+
+        Optional<Customers> customerById = this.customerRepository.findCustomerById(customers.getId());
+        if(customerById.isPresent()) throw new CustomerExitsException("Customer already Exits with this id : "+ customers.getId());
+        return this.customerRepository.save(customers);
+    }
+}
