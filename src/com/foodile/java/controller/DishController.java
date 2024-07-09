86fb493e6ae6298eecdc6cfc03541a1ac2961b03
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-07-09 17:53:20 +0530

    Creating a DishMenu

--- src/com/foodile/java/controller/DishController.java
+++ src/com/foodile/java/controller/DishController.java
@@ -1,5 +1,7 @@
 package com.foodile.java.controller;
 
+import com.foodile.java.exceptions.DishExistsException;
+import com.foodile.java.exceptions.DishNotFoundException;
 import com.foodile.java.model.Dish;
 import com.foodile.java.service.DishServiceImpl;
 
@@ -12,7 +14,23 @@ public class DishController {
         this.dishService = dishService;
     }
 
-    public List<Dish> getDishesList(){
-        return this.dishService.getDishList();
+    public List<Dish> getDisesList(){
+        return this.dishService.getDishesList();
+    }
+
+    public Dish save(Dish dish) throws DishExistsException {
+        return this.dishService.save(dish);
+    }
+
+    public Dish getDishById(String id) throws DishNotFoundException {
+        return this.dishService.getDishById(id);
+    }
+
+    public Dish update(Dish dish) throws DishNotFoundException {
+        return this.dishService.update(dish);
+    }
+
+    public void deleteDish(String id) throws DishNotFoundException{
+        this.dishService.delete(id);
     }
 }
