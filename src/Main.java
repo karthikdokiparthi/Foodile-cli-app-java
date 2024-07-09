9d6ee9e3eca898a22f128c509b2bf1e8c1e352a2
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-07-09 17:59:34 +0530

    Creating a Main class to control over all application in it

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import com.foodile.java.ui.Menu;
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
-        menu.displayMenu();;
+        menu.displayMenu();
 
     }
 }
