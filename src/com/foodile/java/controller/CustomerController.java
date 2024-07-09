926479ab9791881ad161d0375cd4a62cb14d8ba9
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-07-09 17:51:05 +0530

    Updating Customer Class

--- src/com/foodile/java/controller/CustomerController.java
+++ src/com/foodile/java/controller/CustomerController.java
@@ -1,17 +1,45 @@
 package com.foodile.java.controller;
 
 import com.foodile.java.exceptions.CustomerExistsException;
+import com.foodile.java.exceptions.CustomerNotFoundException;
 import com.foodile.java.model.Customers;
 import com.foodile.java.service.CustomerServiceImpl;
 
+import java.util.List;
+
 public class CustomerController {
-    private CustomerServiceImpl customerService;
+    private final CustomerServiceImpl customerService;
 
     public CustomerController(CustomerServiceImpl customerService) {
         this.customerService = customerService;
     }
 
-    public Customers save(Customers customers) throws CustomerExistsException{
-        return this.customerService.save(customers);
+    public Customers save(Customers customer) throws CustomerExistsException {
+        return this.customerService.save(customer);
+    }
+
+    public Customers validateCustomerLogin(String email, String password) throws CustomerNotFoundException {
+        Customers customer = this.customerService.validateCustomerLogin(email, password);
+        if(customer != null)
+            this.customerService.setCurrentLoggedInCustomer(customer);
+        return customer;
+    }
+
+    public Customers currentLoggedInCustomer(){
+        return this.customerService.getCurrentLoggedInCustomer();
+    }
+
+    public Customers getCustomerById(String id) throws CustomerNotFoundException{
+        return this.customerService.getCustomerById(id);
+    }
+
+    public List<Customers> getCustomersList(){ return this.customerService.getAllCustomers();}
+
+    public Customers updateCustomer(Customers customer) throws CustomerNotFoundException {
+        return this.customerService.updateCustomer(customer);
+    }
+
+    public void deleteCustomer(String id) throws CustomerNotFoundException{
+        this.customerService.deleteCustomer(id);
     }
 }
--- src/com/foodile/java/service/CustomerService.java
+++ src/com/foodile/java/service/CustomerService.java
@@ -1,8 +1,25 @@
 package com.foodile.java.service;
 
 import com.foodile.java.exceptions.CustomerExistsException;
+import com.foodile.java.exceptions.CustomerNotFoundException;
 import com.foodile.java.model.Customers;
 
+import java.util.List;
+
 public interface CustomerService {
-    public Customers save(Customers customers) throws CustomerExistsException;
+    public Customers save(Customers customer) throws CustomerExistsException;
+
+    public List<Customers> getAllCustomers();
+
+    public Customers getCustomerById(String id) throws CustomerNotFoundException;
+
+    public Customers updateCustomer(Customers customer) throws CustomerNotFoundException;
+
+    public void deleteCustomer(String id) throws CustomerNotFoundException;
+
+    public Customers validateCustomerLogin(String email, String password) throws CustomerNotFoundException;
+
+    public void setCurrentLoggedInCustomer(Customers customer);
+
+    public Customers getCurrentLoggedInCustomer();
 }
